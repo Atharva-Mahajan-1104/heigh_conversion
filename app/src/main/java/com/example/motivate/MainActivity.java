94504c82aaa9175ba7c48bb1b1479e6674cf97fd
Atharva Mahajan <atharvamahajan2004@gmail.com>
2024-01-21 22:54:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/motivate/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.motivate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+TextView t;
+EditText e;
+Button b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        e=findViewById(R.id.edt);
+        b=findViewById(R.id.btn);
+        t=findViewById(R.id.txt1);
+         b.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                String input=e.getText().toString();
+                Double cm=Double.parseDouble(input);
+                 Double ans =convert(cm);
+                t.setText(""+ans.intValue());
+             }
+         });
+
+
+
+
+    }
+public static double convert(double h){
+    return  h/2.54;
+}
+}
